4028aafda96e0ef9d3efb82256e3dca4bb518866
Bradley Olson <iam@brad.io>
2017-04-13 16:41:59 -0500

    some commenting

--- image_processor/processor.go
+++ image_processor/processor.go
@@ -19,6 +19,8 @@ const MAX_WIDTH = 240
 const MAX_HEIGHT = 240
 const ASCIIMAP = "MN#80Z$ZY?+=~:,. "
 
+// turns pixel into a greyscale value, returning its
+// position in the 0-255 color scale
 func processPixel(px color.Color) uint64 {
 	discolored := color.GrayModel.Convert(px)
 	value := reflect.ValueOf(discolored).FieldByName("Y")
@@ -26,18 +28,21 @@ func processPixel(px color.Color) uint64 {
 	return value.Uint()
 }
 
+// determines a values range in the 0-255 scale
+// and returns the value of its position in ASCIIMAP
 func charPosFromValue(value uint64) byte {
 	pos := int(value * 16 / 255)
 
 	return ASCIIMAP[pos]
 }
 
+// resizes an image, preserving its aspect ratio
 func scaleImage(img image.Image) image.Image {
-	// set lower value to 0
-	// for resize to aspect preserve ratio
 	w := img.Bounds().Max.X
 	h := img.Bounds().Max.Y
 
+	// set lower value to 0
+	// for resize to aspect preserve ratio
 	if w > h {
 		h = 0
 		w = MAX_WIDTH
@@ -51,6 +56,8 @@ func scaleImage(img image.Image) image.Image {
 	return img
 }
 
+// converts an images pixels into a corresponding
+// ascii character, storing it to AsciiImage.Image
 func (ai *AsciiImage) convert(img image.Image) {
 	var buffer bytes.Buffer
 
@@ -66,6 +73,7 @@ func (ai *AsciiImage) convert(img image.Image) {
 	ai.Image = buffer.String()
 }
 
+// create a new AsciiImage from an existing image
 func NewAsciiImage(src image.Image) *AsciiImage {
 	bounds := src.Bounds().Max
 	asciiImage := &AsciiImage{
